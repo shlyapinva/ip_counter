fec6956d17bcb58439d5a820883a7e6cb2766596
shlyapinva <67260426+shlyapinva@users.noreply.github.com>
2020-06-30 14:10:32 +0400

    Update ip_count.java

--- ip_count.java
+++ ip_count.java
@@ -1,50 +1,73 @@
 import java.io.*;
 import java.util.BitSet;
 
-
+//8000000000 rows processed. 1000000000 unique ip-address. 1083740 ms
  public class ip_count{
      public static void main(String[] args) throws IOException {
 
         long timestart = System.currentTimeMillis();
         
-	String filename = args.length < 1 ? "ip_list.txt": args[0];
+        if(args.length < 1)  System.out.println("NAME\n ip_count - Print ip-address counts for file \n SYNOPSIS \n ip_count [file].\n");
 
-	Integer counter=0; //Кол-во уникальных ip
+        long counter=0; // кол-во уникальных ip
+        long n=1; // кол-во обработанных строк ip
+        
+        int[] a = {0,0,0,0};// элементы ip адреса (a1.a2.a3.a4)
+        int j=0; // индекс массива "int[] a = {0,0,0,0}"
+        
+        BitSet[] m_ip = new BitSet[256]; // массив битов в котором (помечаем попавшиеся ip)
+        int x, y; // координаты ip в массиве битов "m_ip"
 
-	Integer  x=-1,y=-1,z=-1,t=-1; //индексы 4-х мерного массива (элементы ip адреса x.y.z.t)
-	BitSet[][][] m_ip = new BitSet[256][256][256]; //4-х мерный массив битов (помечаем попавшиеся ip)
-	
-        // Открываем файл будет автоматически закрыт по ошибке
-        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) { 
+        char c; // символ прочитанный из файла
+        
+        //Открываем файл
+        try (BufferedReader reader = new BufferedReader(new FileReader(args[0]))) { 
             String line;
-            String[] s;
-            for (int n = 1; (line = reader.readLine()) != null; ++n) {
-               
-		s = line.split("[.]+"); //разбиваем строку в массив строк
-		if(s.length<4){
-                    System.out.println(n +": skip '"+line+"'"); 
-                    continue;
-                }
-		x=Integer.valueOf(s[0].trim()); //1-ое число ip x.y.z.t
-		y=Integer.valueOf(s[1].trim()); //2-ое
-		z=Integer.valueOf(s[2].trim()); //3-ое
-		t=Integer.valueOf(s[3].trim()); //4-ое
-				
-		if(m_ip[x][y][z] == null) m_ip[x][y][z]= new BitSet(256);
-		if(!m_ip[x][y][z].get(t)) {
-                    counter++; //если ip не отмечен в массиве увеличим счетчик
-                    m_ip[x][y][z].set(t); //отметим в массиве попавшийся ip
+            int i;
+            
+            // читаем файл построчно
+            for(n=1; (line=reader.readLine())!=null; n++){
+            
+                //переводим поток символов в элементы ip адреса (a1.a2.a3.a4)
+                for (i = 0; i < line.length(); i++) {
+                    c=line.charAt(i);
+                    if (c == '.'){j++; continue;}
+                    if(c<'0'||c>'9') continue;
+                    if(j>3) continue;
+                    a[j]=10*a[j]+(c-'0');
+ //                   System.out.println(a[0]);
                 }
                 
-                //наблюдаем процесс каждые 10000000 строк
-		if(n % 10000000==0) {
+                //проверим  ip адрес 
+                if(j==3){
+                    //вычислим координаты ip в массиве битов "m_ip"
+                    x=a[0];
+                    y=65536*a[1]+256*a[2]+a[3]; 
+        
+                    //Выделим память для массив битов
+                    if(m_ip[x] == null) 
+                        m_ip[x]= new BitSet(16777216);
+                    
+                    //если попавшийся ip не отмечен в массиве
+                    if( !m_ip[x].get(y) ) {
+                        counter++; // увеличим счетчик 
+                        m_ip[x].set(y); //отметим ip в массиве
+                    }
+                }
+                 
+                //Обнуляем переменные
+                a[0] =0; a[1] =0; a[2] =0; a[3] =0; 
+                j=0; 
+            
+                //наблюдаем процесс каждые 100000000 строк
+                if(n % 100000000==0) 
                     System.out.println(n + " rows processed. "+counter+" unique ip-address. "+(System.currentTimeMillis()-timestart)+" ms"); 
-                }				
             }
-        }catch (FileNotFoundException e) {
-            System.err.println("File not found.");
+            
+        } catch (FileNotFoundException e) {
+            System.err.println("Cannot open file.");
         }
-		
-	System.err.println("In all: "+counter+" unique ip-address in the file" );
+        
+        System.out.println("In all: "+n+ " rows processed. "+counter+" unique ip-address. "+(System.currentTimeMillis()-timestart)+" ms"); 
     }
  }
